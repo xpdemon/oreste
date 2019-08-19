5a3c521036b222c0bbc9b77ea9d96b691255f6b3
xpdemon <34739132+xpdemon@users.noreply.github.com>
2019-08-19 16:53:51 +0200

    css

--- src/app/joueurs/joueurs.component.ts
+++ src/app/joueurs/joueurs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ElasticService } from '../shared/elastic.service';
 import { JoueurSource } from './joueur.Interface';
 import { GameSource } from '../models/game';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-joueurs',
@@ -13,19 +14,40 @@ export class JoueursComponent implements OnInit {
   private static readonly INDEX = 'joueurs';
   joueurSources: JoueurSource[];
   gamesources: GameSource[];
-  constructor(private es: ElasticService) { }
+  form: FormGroup;
+  constructor(private es: ElasticService) {
+    this.form = new FormGroup({
+      pseudo: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit() {
     this.getAlljoueurs();
   }
 
-  getAlljoueurs(){
+  getAlljoueurs() {
     this.es.getAllDocuments(JoueursComponent.INDEX)
-    .then(response => {
-      this.joueurSources = response.hits.hits;
-      console.log(response);
+      .then(response => {
+        this.joueurSources = response.hits.hits;
+        console.log(response);
+      }, error => {
+        console.log(error);
+      });
+  }
+
+
+  onSubmit(value) {
+    this.es.createDoc({
+      index: 'joueurs',
+      id: value.pseudo,
+      body: {
+        pseudo: value.pseudo
+      }
+    }).then((result) => {
+      console.log(result);
+      window.location.reload();
     }, error => {
-      console.log(error);
+      console.error(error);
     });
   }
 }
