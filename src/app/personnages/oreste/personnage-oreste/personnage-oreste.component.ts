3b430fd9457431483ecf29a6230a6c5e74cadf01
xpdemon <34739132+xpdemon@users.noreply.github.com>
2019-08-26 17:06:43 +0200

    afficherperso

--- src/app/personnages/oreste/personnage-oreste/personnage-oreste.component.ts
+++ src/app/personnages/oreste/personnage-oreste/personnage-oreste.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonnageOreste } from './personnage-oreste.interface';
 
 @Component({
   selector: 'app-personnage-oreste',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personnage-oreste.component.scss']
 })
 export class PersonnageOresteComponent implements OnInit {
-
+  personnage: PersonnageOreste;
   constructor() { }
 
   ngOnInit() {
